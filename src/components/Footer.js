0cc21595d4997c2d4f4af16cc95e813c4bf11002
dszarkiel <d.szarkiel@gmail.com>
2021-03-11 19:07:33 -0600

    minor CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -34,50 +34,52 @@ class Footer extends React.Component {
             <Navbar.Collapse id="responsive-navbar-nav">
             <Container fluid>
                 <Col lg={4} className="text-center">
-                    
-                </Col>
-
-                <Col lg={4} className="text-center">
-
                 <Nav.Link onClick={this.handleAboutTeam}>
                          About Team
                     </Nav.Link>
+
                 </Col>
 
                 <Col lg={4} className="text-center">
+                    Copyright &copy; 2021 Capitall
+                </Col>
 
+                <Col lg={4} className="text-center">
+                    Privacy Policy 
                 </Col>
             </Container>
             </Navbar.Collapse>
             </Navbar>
 
                 <Modal show={this.state.showModal} dialogClassName="user-view" size="lg">
-                    <Modal.Header closeButton onClick={() => this.handleAboutTeam()}>
-                    <Modal.Title>About Design Team</Modal.Title>
+                    <div className="footer-links">
+                        <Modal.Header closeButton onClick={() => this.handleAboutTeam()}>
+                        <Modal.Title>About Design Team</Modal.Title>
 
-                    </Modal.Header>
-                    <Modal.Body>
-                        <ListGroup.Item>
-                        <h1 className="designer-link" onClick={() => this.openInNewTab("https://github.com/dszarkiel")}>
-                            Github <Github/>
-                        </h1>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        <h1 className="designer-link" onClick={() => this.openInNewTab("https://www.linkedin.com/in/dominikszarkiel/")}>
-                            LinkedIn <Linkedin/>
-                        </h1>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                        <h1 className="designer-link"  onClick={() => this.openInNewTab("https://www.youtube.com/watch?v=HMUNP0uMqzs&t=2s")}>
-                            Demo <CameraReelsFill/>
-                        </h1>
-                        </ListGroup.Item>
-                    </Modal.Body>
-                    <Modal.Footer>
-                    <Button variant="secondary" onClick={() => this.handleAboutTeam()}>
-                        Close
-                    </Button>
-                    </Modal.Footer>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <ListGroup.Item>
+                            <h1 className="designer-link" onClick={() => this.openInNewTab("https://github.com/dszarkiel")}>
+                                Github <Github/>
+                            </h1>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                            <h1 className="designer-link" onClick={() => this.openInNewTab("https://www.linkedin.com/in/dominikszarkiel/")}>
+                                LinkedIn <Linkedin/>
+                            </h1>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                            <h1 className="designer-link"  onClick={() => this.openInNewTab("https://www.youtube.com/watch?v=HMUNP0uMqzs&t=2s")}>
+                                Demo <CameraReelsFill/>
+                            </h1>
+                            </ListGroup.Item>
+                        </Modal.Body>
+                        <Modal.Footer>
+                        <Button variant="secondary" onClick={() => this.handleAboutTeam()}>
+                            Close
+                        </Button>
+                        </Modal.Footer>
+                    </div>
                 </Modal>
                 </footer>
             </div>
